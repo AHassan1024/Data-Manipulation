b778df4d73b2b9c61d61603c37839a3880aa57c0
Ameena Hassan <ameenahassan1024@gmail.com>
2023-07-21 13:17:58 +0100

    Skeleton insights

---
+++ insights.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Load the flight data from the CSV file
+# Display the first few rows of the dataset
+# Check the number of rows and columns in the dataset
+# Check data types and non-null counts for each field
+# Summary statistics for numeric columns
+# Check for missing values in each column
